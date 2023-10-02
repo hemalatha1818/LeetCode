49e0b5f5eac0b915c961c05afd8fdcb2e0b279b9
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2023-10-03 00:01:07 +0530

    Time: 58 ms (24.05%), Space: 16.1 MB (98.98%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -11,33 +11,10 @@ class Solution:
         d["C"]=100
         d["D"]=500
         d["M"]=1000
-        i=len(s)-1
-        while(i>=0):
-            
-            if s[i]=="I" and s[i-1]=="I":
-                st=st+d[s[i]]
-                i=i-1
-            elif i-1>=0 and (s[i]=="X" or s[i]=="V") and s[i-1]=="I":
-                st=st+d[s[i]]-1
-                i=i-2
-            elif i-1>=0 and (s[i-1]=="X" or s[i-1]=="V") and s[i]=="I":
-                st=st+d[s[i-1]]+1
-                i=i-2
-            
-            elif i-1>=0 and (s[i]=="L" or s[i]=="C") and s[i-1]=="X":
-                    y=s[i-1]
-                    st=st+d[s[i]]-10
-                    i=i-2
-            elif i-1>=0 and (s[i-1]=="L" or s[i-1]=="C") and s[i]=="X":
-                st=st+d[s[i-1]]+10
-                i=i-2
-            elif i-1>=0 and (s[i]=="D" or s[i]=="M") and s[i-1]=="C":
-                st=st+d[s[i]]-100
-                i=i-2
-            elif i-1>=0 and (s[i-1]=="D" or s[i-1]=="M") and s[i]=="C":
-                st=st+d[s[i-1]]+100
-                i=i-2
-            else:
-                st=st+d[s[i]]
-                i=i-1
+        s=s.replace("IV","IIII").replace("IX","VIIII")
+        s=s.replace("XL","XXXX").replace("XC","LXXXX")
+        s=s.replace("CD","CCCC").replace("CM","DCCCC")
+        for i in s:
+            st+=d[i]
+        
         return st
