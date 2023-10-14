647d18ce43311c1be96e6e548d8e38f982e5ed57
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2023-10-14 19:40:26 +0530

    Time: 247 ms (90.28%), Space: 18.8 MB (94.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,15 @@
 from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
         sum=0
-        
-      
-        d=defaultdict(int)
+        d={0:1}
         c=0
-        d[0]=1
         for i in range(len(nums)):
-            sum+=nums[i]
-            c+=d[sum-k]
-            d[sum]+=1
+            sum=sum+nums[i]
+            if sum-k in d:
+                c=c+d[sum-k]
+            if sum not in d:
+                d[sum]=1
+            else:
+                d[sum]+=1
         return c
