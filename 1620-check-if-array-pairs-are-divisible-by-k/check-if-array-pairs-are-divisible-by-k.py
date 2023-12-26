41a4ce80e1e10246032c80696394ff757c9280cf
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2023-12-26 21:54:12 +0530

    Time: 506 ms (95.36%) | Memory: 31.3 MB (9.28%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -2,16 +2,18 @@ class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
         d={}
         for i in arr:
-            if (i+k)%k in d:
-                d[(i+k)%k]+=1
+            if i%k in d:
+                d[i%k]+=1
             else:
-                d[(i+k)%k]=1
-    
+                d[i%k]=1
         for i in d:
-            if i==0 and d[i]%2==0:
-                return True
-            elif  k-i not in d:
-                return False
-            elif d[i]!=d[k-i]:
-                return False
+                if i==0:
+                    if d[i]%2!=0:
+                        return False
+                elif k-i not in d:
+                    return False
+                elif d[i]!=d[k-i]:
+                    return False
         return True
+        
+        
