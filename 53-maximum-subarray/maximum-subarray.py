3fc0e93cd1dfd622fd5934ad53611eda462aa9ad
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-02-27 22:11:17 +0530

    Time: 518 ms (77.36%) | Memory: 30.8 MB (89.90%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -5,10 +5,11 @@ class Solution:
         if len(nums)==1:
             return nums[0]
         for i in range(len(nums)):
-            
-            if sum<0:
-               sum=0
             sum+=nums[i]
             maxi=max(maxi,sum)
+            if sum<0:
+               sum=0
+            
+            
         return maxi
     
