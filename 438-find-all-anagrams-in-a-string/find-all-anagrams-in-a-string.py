9488fe629a68441eef728a9d266c77ce4c1272fe
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-03-02 11:23:08 +0530

    Time: 88 ms (67.50%) | Memory: 17.8 MB (58.76%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,32 +1,32 @@
 from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        #BruteForce
-        if len(p)>len(s):
-            return ""
-        da=[0]*26
-        pa=[0]*26
+        x=""
         l=[]
+        sa=[0]*26
+        sp=[0]*26
+        x=""
+        l=[]
+        sa=[0]*26
+        sp=[0]*26
+        j=0
         for i in p:
-            pa[ord(i)-ord("a")]+=1
-        
-        for i in range(len(p)):
-            da[ord(s[i])-ord('a')]+=1
-        
-        f=1    
-        for i in range(len(pa)):
-            if(pa[i]!=da[i]):
-                f=0
-        if f==1:
-            l.append(0)
-        f=1       
-        for k in range(len(p),len(s)):
-            f=1
-            da[ord(s[k-len(p)])-ord("a")]-=1
-            da[ord(s[k])-ord("a")]+=1
-            for i in range(len(pa)):
-                if(pa[i]!=da[i]):
-                    f=0
-            if f==1:
-                l.append(k-len(p)+1)
+            sp[ord(i)-ord('a')]+=1
+        for i in range(len(s)):
+            sa[ord(s[i])-ord('a')]+=1
+
+            if sa==sp:
+                l.append(i-len(p)+1)
+            
+            while(sa>=sp and j<i):
+                if sa[ord(s[j])-ord('a')]>=1:
+                    sa[ord(s[j])-ord('a')]-=1
+                
+                
+                if sa==sp:
+                    l.append(i-len(p)+1)
+                j+=1
         return l
+
+            
+        
