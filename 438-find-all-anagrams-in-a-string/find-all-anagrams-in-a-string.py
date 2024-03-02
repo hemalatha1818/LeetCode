d61f8d88354306182f466a60695a180275035329
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-03-02 12:21:37 +0530

    Time: 72 ms (91.88%) | Memory: 18 MB (39.56%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -10,22 +10,25 @@ class Solution:
         sa=[0]*26
         sp=[0]*26
         j=0
+        if len(p)>len(s):
+            return ""
         for i in p:
             sp[ord(i)-ord('a')]+=1
-        for i in range(len(s)):
+        for i in range(len(p)):
             sa[ord(s[i])-ord('a')]+=1
 
             if sa==sp:
                 l.append(i-len(p)+1)
+        
+        for i in range(len(p),len(s)):
+            if sa[ord(s[j])-ord('a')]>=1:
+                sa[ord(s[j])-ord('a')]-=1
+            sa[ord(s[i])-ord('a')]+=1
+        
             
-            while(sa>=sp and j<i):
-                if sa[ord(s[j])-ord('a')]>=1:
-                    sa[ord(s[j])-ord('a')]-=1
-                
-                
-                if sa==sp:
-                    l.append(i-len(p)+1)
-                j+=1
+            if sa==sp:
+                l.append(i-len(p)+1)
+            j+=1
         return l
 
             
