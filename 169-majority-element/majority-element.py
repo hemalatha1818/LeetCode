47d0d768e635e7285a09bb9dff64223745c10528
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-03-11 09:33:47 +0530

    Time: 179 ms (9.80%) | Memory: 17.9 MB (79.73%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #brute force
+        d={}
+        for i in nums:
+            d[i]=d.get(i,0)+1
+        for i in d:
+            if d[i]>len(nums)//2:
+                return i
