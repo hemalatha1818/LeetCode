b0f5aab01661ce61f4f2653c5e46c3c808b47a3d
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-03-16 10:26:50 +0530

    Time: 42 ms (68.31%) | Memory: 16.9 MB (94.25%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,8 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        mid=0
         low=0
-        arr=nums.copy()
         high=len(nums)-1
         while low<=high:
             mid=(low+high)//2
@@ -13,11 +11,9 @@ class Solution:
                     high=mid-1
                 else:
                     low=mid+1
-                
             else:
                 if nums[mid]<=target and target<=nums[high]:
                     low=mid+1
                 else:
                     high=mid-1
-
         return -1
