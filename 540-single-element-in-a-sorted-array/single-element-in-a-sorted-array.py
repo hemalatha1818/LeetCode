1dc99f4400f234acb49ca3493a4d62c57124ff44
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-03-28 17:46:06 +0530

    Time: 159 ms (5.22%) | Memory: 26.5 MB (90.36%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,12 +1,16 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
         for i in range(len(nums)):
-            if i>0 and i<len(nums)-1 and nums[i-1]!=nums[i] and nums[i]!=nums[i+1]:
-                return nums[i]
-            elif i<=0 and i<len(nums)-1 and nums[i+1]!=nums[i]:
-                return nums[i]
-            elif i>=len(nums)-1 and nums[i-1]!=nums[i]:
-                return nums[i]
-        return nums[0]
+            if i==0 :
+                if nums[i+1]!=nums[i]:
+                    return nums[0]
+            elif i==len(nums)-1:
+                if nums[i-1]!=nums[i]:
+                    return nums[i]
+            else:
+                if nums[i+1]!=nums[i] and nums[i-1]!=nums[i]:
+                    return nums[i]
 
         
