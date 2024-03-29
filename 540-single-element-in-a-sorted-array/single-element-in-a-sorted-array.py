c1cec0af8951a5bb0eef85d1696886c0b82e0f8e
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-03-29 22:15:39 +0530

    Time: 147 ms (20.84%) | Memory: 26.5 MB (90.05%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -2,21 +2,21 @@ class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         if len(nums)==1 or nums[0]!=nums[1]:
             return nums[0]
-        elif nums[len(nums)-1]!=nums[len(nums)-2]:
+        if nums[len(nums)-1]!=nums[len(nums)-2]:
             return nums[len(nums)-1]
-        else:
-            low=1
-            high=len(nums)-2
-            while low<=high:
-                mid=(low+high)//2
-                if nums[mid-1]!=nums[mid] and nums[mid]!=nums[mid+1]:
-                    return nums[mid]
+        
+        low=1
+        high=len(nums)-2
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid-1]!=nums[mid] and nums[mid]!=nums[mid+1]:
+                return nums[mid]
+            else:
+                if mid%2==0 and nums[mid+1]==nums[mid] or mid%2==1 and nums[mid-1]==nums[mid]:
+                    low=mid+1
                 else:
-                    if mid%2==0 and nums[mid+1]==nums[mid] or mid%2==1 and nums[mid-1]==nums[mid]:
-                        low=mid+1
-                    else:
-                        high=mid-1
-            return -1
+                    high=mid-1
+        return -1
                 
             
 
