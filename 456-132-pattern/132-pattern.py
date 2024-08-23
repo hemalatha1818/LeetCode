4d5ea5d0ee9c94086eb2d43523d73099eef0e47e
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-08-23 19:07:32 +0530

    Time: 533 ms (58.86%) | Memory: 37.9 MB (5.20%) - LeetSync

---
+++ 456-132-pattern/132-pattern.py
@@ -0,0 +1,16 @@
+import sys
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        nums_k=float(-inf)
+        stack=[]
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]<nums_k:
+                return True
+            while stack and stack[len(stack)-1]<nums[i]:
+                nums_k=stack.pop()
+            
+            stack.append(nums[i])
+        return False
+
+                
+        
