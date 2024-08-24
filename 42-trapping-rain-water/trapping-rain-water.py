21d0e6d4c301e3f5445afbcf4892c340d3967ca6
HemaLatha <69495079+hemalatha1818@users.noreply.github.com>
2024-08-24 13:03:23 +0530

    Time: 128 ms (15.62%) | Memory: 18.6 MB (23.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -3,18 +3,16 @@ class Solution:
         leftmax=[0]*len(height)
         rightmax=[0]*len(height)
         t=0
-        for i in range(len(height)):
-            if i==0:
-                leftmax[i]=height[0]
-            else:
-                leftmax[i]=max(leftmax[i-1],height[i])
+        
         for i in range(len(height)-1,-1,-1):
             if i==len(height)-1:
                 rightmax[i]=height[len(height)-1]
             else:
                 rightmax[i]=max(rightmax[i+1],height[i])
+        leftmax=-999999
         for i in range(len(height)):
-            mini=min(leftmax[i],rightmax[i])
+            leftmax=max(height[i],leftmax)
+            mini=min(leftmax,rightmax[i])
             t+=mini-height[i]
         return t
 
